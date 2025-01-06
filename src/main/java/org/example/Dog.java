693bf0063e24bc84e9f6ab323aedafe4bae5aacf
Anton <toropow@gmail.com>
2025-01-06 19:59:28 +0300

    Add new annotations

--- src/main/java/org/example/Dog.java
+++ src/main/java/org/example/Dog.java
@@ -1,12 +1,21 @@
 package org.example;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
 public class Dog implements Pet {
     private String name;
 
+    @PostConstruct
     public void init(){
         System.out.println("Class Dod: init method");
     }
 
+    @PreDestroy
     public void destroy(){
         System.out.println("Class Dod: destroy method");
     }
