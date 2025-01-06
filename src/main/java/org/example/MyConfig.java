22f5802045f229f4a74dee16362a994749d9e2b1
Anton <toropow@gmail.com>
2025-01-06 20:46:09 +0300

    Add new annotations

--- src/main/java/org/example/MyConfig.java
+++ src/main/java/org/example/MyConfig.java
@@ -1,10 +1,20 @@
 package org.example;
 
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 @Configuration
-@ComponentScan("org.example")
+//@ComponentScan("org.example")
+@PropertySource("classpath:myApp.properties")
 public class MyConfig {
+    @Bean
+    @Scope("singleton")
+    public Pet catBean(){
+        return new Cat();
+    }
+
+    @Bean
+    public Person personBean(){
+        return new Person(catBean());
+    }
 }
--- src/main/java/org/example/Test6.java
+++ src/main/java/org/example/Test6.java
@@ -6,8 +6,14 @@ public class Test6 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
+//        Pet cat1 = context.getBean("catBean", Pet.class);
+//        Pet cat2 = context.getBean("catBean", Pet.class);
+//        cat1.say();
+
        Person person = context.getBean("personBean", Person.class);
-       person.callYourPet();
+        System.out.println(person.getAge());
+        System.out.println(person.getSurname());
+//       person.callYourPet();
 
        context.close();
     }
