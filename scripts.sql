f7058cb6cc7e19c7782e04c86e02b07b45ae4e01
Anton <toropow@gmail.com>
2025-01-22 22:48:18 +0300

    add one-to-one

--- scripts.sql
+++ scripts.sql
@@ -1,2 +1,19 @@
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO bestuser;
 GRANT USAGE ON SEQUENCE employees_id_seq TO bestuser;
+
+CREATE TABLE details (
+                         id SERIAL PRIMARY KEY,
+                         city varchar(15),
+                         phone_number varchar(25),
+                         email varchar(30)
+);
+
+CREATE TABLE employees (
+                           id SERIAL PRIMARY KEY,
+                           name varchar(15),
+                           surname varchar(25),
+                           department varchar(20),
+                           salary int,
+                           details_id int,
+                           FOREIGN KEY (details_id) REFERENCES details(id)
+);
---
+++ src/main/java/hibernate_test2/Test1.java
@@ -0,0 +1,44 @@
+package hibernate_test2;
+
+import hibernate_test2.entity.Detail;
+import hibernate_test2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.config.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+//            Employee employee = new Employee("Oleg", "Smirnov", "Sales", 1000);
+//            Detail detail = new Detail("Piter", "799952212", "olejka@tdf.com");
+//            employee.setEmpDetail(detail);
+//            session.beginTransaction();
+//            session.save(employee);
+
+
+//            session.beginTransaction();
+//            Employee emp = session.get(Employee.class, 1);
+//            System.out.println(emp.getEmpDetail());
+
+            session.beginTransaction();
+            Employee emp = session.get(Employee.class, 2);
+            session.delete(emp);
+
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
